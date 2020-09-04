20796200e5974965145758a847e5c5269d590ed4
irahulcse <1rahulchandra1@gmail.com>
2020-09-04 08:51:10 +0530

    Good First Commit at 2020-09-04-08:51:10

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,11 @@ const Home = () => (
 
     <HeroSection />
     <HomeBanner />
-    <CategoryBanner />
+    {/* <CategoryBanner /> */}
     <ProductsBanner />
-    <ExploreBanner />
+    {/* <ExploreBanner />
     <SocialMedia />
-    <Footer />
+    <Footer /> */}
   </Root>
 );
 
